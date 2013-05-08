5e6139105e0a57088bc31d306434c3a08561eeba
Mohamed Adam Chaieb <m.adam-chaieb@live.com>
2013-05-08 01:46:04 -0400

    minor fix to Mathematics class (removed misplaced code)

--- Mathematics.java
+++ Mathematics.java
@@ -7,52 +7,5 @@ import java.math.BigDecimal;
 
 public class Mathematics
 {
-  //Int functions
 
-  
-  //Real functions
-  public Real add(Real a, Real b)
-  {
-    return new Real(a.real.add(b.real));
-  }
-  
-  public Real multiply(Real a, Real b)
-  {
-    return new Real(a.real.multiply(b.real));
-  }
-  
-  public Real subtract(Real a, Real b)
-  {
-    return new Real(a.real.subtract(b.real));
-  }
-  
-  public Real divide(Real a, Real b, int scale)
-  {
-    try{BigDecimal output = a.real.divide(b.real); return new Real(output);}
-    catch(Exception e)
-    {
-      BigDecimal output = a.real.divide(b.real, scale, BigDecimal.ROUND_HALF_UP); 
-      return new Real(output);
-    }
-  }
-  
-  public Real mod(Real a, Real b)
-  {
-    return new Real(a.real.remainder(b.real));
-  }
-  
-  public Real negate(Real a)
-  {
-    return new Real(a.real.negate());
-  }
-  
-  public Real max(Real a, Real b)
-  {
-    return new Real(a.real.max(b.real));
-  }
-  
-  public Real min(Real a, Real b)
-  {
-    return new Real(a.real.min(b.real));
-  }
 }
